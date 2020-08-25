056a4d9a7581ef2d8ee622ca703f7fb7d8ce653a
PaulZhuzliakov <paulparatrooper@gmail.com>
2020-08-25 18:10:02 +0300

    добавлен функционал на кнопку копирования
    испралена NullPointerException при попытке подняться выше корневого каталога

--- src/main/java/fileManager/Controller.java
+++ src/main/java/fileManager/Controller.java
@@ -4,9 +4,7 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.util.Callback;
 
@@ -15,7 +13,7 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.nio.file.StandardCopyOption;
 import java.util.Comparator;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -32,6 +30,7 @@ public class Controller implements Initializable {
     //нужно запоминать текущий адрес
     Path root;
 
+    Path selectedCopyFile;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -81,25 +80,24 @@ public class Controller implements Initializable {
         root = path;
         //путь прописывается в pathField`е
         pathField.setText(root.toAbsolutePath().toString());
-        //еред тем как заполнять коллекцию, её нужно почистить
+        //перед тем как заполнять коллекцию, её нужно почистить
         filesList.getItems().clear();
         //переход на уровень выше. Будет выше директорий, потому что размер ещё меньше
         filesList.getItems().add(new FileInfo(FileInfo.UpToken, -2L));
-
         //список файлов обновляется на соответствующую папку
         filesList.getItems().addAll(scanFiles(path));
         filesList.getItems().sort(new Comparator<FileInfo>() {
             @Override
             //лексографическая сортировка папок и файлов
-            // компаратор возвращает 1, если первое число больше второго; -1 если наоборот. 0 если равны;
+            //компаратор возвращает 1, если первое число больше второго; -1 если наоборот. 0 если равны;
             public int compare(FileInfo f1, FileInfo f2) {
                 if (f1.getFileName().equals(FileInfo.UpToken))
                     return -1;
                 //если знаки одинаковые(два положительных или два отрицательных), то это две папки или два файла
-                if ((int)Math.signum(f1.getLength()) == (int)Math.signum(f2.getLength())) {
+                if ((int) Math.signum(f1.getLength()) == (int) Math.signum(f2.getLength())) {
                     return f1.getFileName().compareTo(f2.getFileName());
                 }
-                return (int) (f1.getLength()-f2.getLength());
+                return (int) (f1.getLength() - f2.getLength());
             }
         });
     }
@@ -134,21 +132,55 @@ public class Controller implements Initializable {
 
     //переход в пвпку
     public void filesListClicked(MouseEvent mouseEvent) {
-         if (mouseEvent.getClickCount() ==2) {
-             FileInfo fileInfo = filesList.getSelectionModel().getSelectedItem();
-             //если двойной клик не был сделан в пустое пространство
-             if (fileInfo != null) {
-                 if (fileInfo.isDirectory()) {
-                     Path pathTo = root.resolve(fileInfo.getFileName());
-                     goToPath(pathTo);
-                 }
-                 if (fileInfo.isUpElement()) {
-                     Path pathTo = root.toAbsolutePath().getParent();
-                     goToPath(pathTo);
-                 }
-             }
-         }
+        if (mouseEvent.getClickCount() == 2) {
+            FileInfo fileInfo = filesList.getSelectionModel().getSelectedItem();
+            //если двойной клик не был сделан в пустое пространство
+            if (fileInfo != null) {
+                if (fileInfo.isDirectory()) {
+                    Path pathTo = root.resolve(fileInfo.getFileName());
+                    goToPath(pathTo);
+                }
+                if (fileInfo.isUpElement()) {
+                    if (root.getParent() != null) {
+                        Path pathTo = root.toAbsolutePath().getParent();
+                        goToPath(pathTo);
+                    }
+                }
+            }
+        }
     }
 
+    //обновление списка файлов. По сути - переход в текущий каталог
+    public void refresh() {
+        goToPath(root);
+    }
 
+    public void copyAction(ActionEvent actionEvent) {
+        FileInfo fileInfo = filesList.getSelectionModel().getSelectedItem();
+        if (selectedCopyFile == null && (fileInfo == null || fileInfo.isDirectory() || fileInfo.isUpElement())) {
+            return;
+        }
+        //.getSource() - В событии есть информация о том элементе, который вызвал это событие
+        //каст события к Button
+        if (selectedCopyFile == null) {
+            selectedCopyFile = root.resolve(fileInfo.getFileName());
+            ((Button) actionEvent.getSource()).setText("Копируется: " + selectedCopyFile);
+            return;
+        }
+        //если этот файл ести и кнопку нажзимают ещё раз
+        if (selectedCopyFile != null) {
+            //перезаписать существующий в случае совпадения
+            try {
+                //опируем не просто в папку, а в папку + имя файла .resolve(selectedCopyFile.getFileName())
+                Files.copy(selectedCopyFile, root.resolve(selectedCopyFile.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+                //после копирования очищаем временный Путь
+                selectedCopyFile = null;
+                ((Button) actionEvent.getSource()).setText("Копирование");
+                refresh();
+            } catch (IOException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Невозможно скопировать файл");
+                alert.showAndWait();
+            }
+        }
+    }
 }
